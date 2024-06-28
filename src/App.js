a89a32b5a1feac87f0f4734ee96c7f9c5e3f77aa
mahendraadkri <mahendrachhetri500@gmail.com>
2024-06-28 16:52:58 +0545

    1st Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mahendra How are you ?
         </a>
       </header>
     </div>
